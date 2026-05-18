92ba211136dc426a2ef3acc92ec465bca447c04f
Muegge <marcelgraf79@gmail.com>
2026-05-18 15:42:43 +0200

    logs

--- server.js
+++ server.js
@@ -117,6 +117,7 @@ io.on('connection', (socket) => {
 
   // Neues Game anfragen
   socket.on('new-game-request', ({ roomId }) => {
+    console.log('New game request in room:', roomId);
     socket.to(roomId).emit('new-game-requested');
   });
 
